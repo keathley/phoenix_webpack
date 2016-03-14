cb62d11a32a184880b1984c3413c3c18a64bcde1
Chris Keathley <c@keathley.io>
2016-03-14 14:23:02 -0400

    Basic functionality
    This now allows you to create an incredibly basic webpack configuration
    for a phoenix application. Everything is based on the official phoenix
      documentation

--- lib/mix/tasks/phoenix_webpack.generate.ex
+++ lib/mix/tasks/phoenix_webpack.generate.ex
@@ -1,11 +1,38 @@
 defmodule Mix.Tasks.PhoenixWebpack.Generate do
   use Mix.Task
 
-  def run(_args) do
-    raise PhoenixWebpack.NotImplemented
-  end
-end
+  @webpack "webpack@1.12.12"
 
-defmodule PhoenixWebpack.NotImplemented do
-  defexception message: "This functionality is not implemented yet"
+  @webpack_config """
+  module.exports = {
+    entry: "./web/static/js/app.js",
+    output: {
+      path: "./priv/static/js",
+      filename: "app.js"
+    }
+  };
+  """
+
+  def run(_args) do
+    remove_brunch_config
+    remove_node_modules
+    install_webpack
+    generate_webpack_config
+  end
+
+  defp remove_brunch_config do
+    File.rm "brunch-config.js"
+  end
+
+  defp remove_node_modules do
+    File.rm_rf "node_modules"
+  end
+
+  defp install_webpack do
+    System.cmd("npm", ["install", "--save-dev", @webpack])
+  end
+
+  def generate_webpack_config do
+    File.write("webpack.config.js", @webpack_config)
+  end
 end
