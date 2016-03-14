b641f476f26d838ead22cddae524130bca41cd5f
Chris Keathley <c@keathley.io>
2016-03-14 14:05:21 -0400

    Add hex information

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule PhoenixWebpack.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
@@ -28,5 +29,14 @@ defmodule PhoenixWebpack.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Chris Keathley"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/keathley/phoenix_webpack"}
+    ]
   end
 end
