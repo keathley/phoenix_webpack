0e1b69435fdbcd18b7b771e4bdce07a3af246b16
Chris Keathley <c@keathley.io>
2016-03-14 14:10:23 -0400

    Throw not implemented error

--- lib/mix/tasks/phoenix_webpack.generate.ex
+++ lib/mix/tasks/phoenix_webpack.generate.ex
@@ -2,6 +2,10 @@ defmodule Mix.Tasks.PhoenixWebpack.Generate do
   use Mix.Task
 
   def run(_args) do
-    IO.puts "Hello world"
+    raise PhoenixWebpack.NotImplemented
   end
+end
+
+defmodule PhoenixWebpack.NotImplemented do
+  defexception message: "This functionality is not implemented yet"
 end
