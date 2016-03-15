74913b2d76e7aafe6a077e17c23ae29d5f7b9854
Chris Keathley <c@keathley.io>
2016-03-14 21:52:36 -0400

    Remove brunch stuff and use mix helper functions

--- lib/mix/tasks/phoenix_webpack.generate.ex
+++ lib/mix/tasks/phoenix_webpack.generate.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.PhoenixWebpack.Generate do
   use Mix.Task
 
   @webpack "webpack@1.12.12"
-
+  @webpack_command "npm install --save-dev #{@webpack}"
   @webpack_config """
   module.exports = {
     entry: "./web/static/js/app.js",
@@ -12,27 +12,30 @@ defmodule Mix.Tasks.PhoenixWebpack.Generate do
     }
   };
   """
+  @watcher_command "watchers: [node: [\"node_modules/webpack/bin/webpack.js\", \"--watch\", \"--color\"]]"
 
   def run(_args) do
-    remove_brunch_config
-    remove_node_modules
     install_webpack
     generate_webpack_config
-  end
-
-  defp remove_brunch_config do
-    File.rm "brunch-config.js"
-  end
-
-  defp remove_node_modules do
-    File.rm_rf "node_modules"
+    print_info
   end
 
   defp install_webpack do
-    System.cmd("npm", ["install", "--save-dev", @webpack])
+    Mix.shell.info [:green, "* running ", :reset, @webpack_command]
+    Mix.shell.cmd(@webpack_command, quiet: true)
   end
 
-  def generate_webpack_config do
-    File.write("webpack.config.js", @webpack_config)
+  defp generate_webpack_config do
+    Mix.Generator.create_file("webpack.config.js", @webpack_config)
+  end
+
+  defp print_info do
+    Mix.shell.info """
+
+    Webpack is now installed! To enable live reloading
+    change your watchers inside of `config/dev.exs` to:
+
+        #{@watcher_command}
+    """
   end
 end
